94bba0e943117ae7c37fa8c34bfe5ecc81bcc9d6
iwin <418577121@qq.com>
2021-08-16 09:24:40 +0800

    Initial commit

---
+++ springboot-001/src/main/java/com/iwin/com/HelloController.java
@@ -0,0 +1,20 @@
+package com.iwin.com;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @project_name: learn-springboot
+ * @package_name: com.iwin.com
+ * @description: 测试001
+ * @author: DingHaiTing
+ * @create_time: 2021-08-16 09:16
+ **/
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(String name) {
+        return "hello world " + name;
+    }
+}
