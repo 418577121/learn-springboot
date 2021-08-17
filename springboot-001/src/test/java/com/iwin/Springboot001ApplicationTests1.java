7b71e68c897ea26a48256df1ed64074f12e4cbdc
iwin <418577121@qq.com>
2021-08-17 22:24:17 +0800

    springboo002构建 配置文件优先级

--- springboot-001/src/test/java/com/iwin/Springboot001ApplicationTests1.java
+++ springboot-001/src/test/java/com/iwin/Springboot001ApplicationTests1.java
@@ -4,8 +4,10 @@ import com.iwin.controller.StudentController;
 import com.iwin.entity.Employee;
 import com.iwin.entity.School;
 import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpMethod;
@@ -31,11 +33,25 @@ public class Springboot001ApplicationTests1 {
 
     @Resource
     private Employee employee;
+
+    @Autowired
+    private StringEncryptor encryptor;
     @Test
     public void testProperties() {
         System.out.println(school);
         System.out.println(ioc.containsBean("testService"));
         System.out.println(employee);
+    }
+    @Test
+    public void testPassword() {
+        /**
+         * 测试jasypt加密解密
+         */
+        String password = "清华";
+        String encryptPwd = encryptor.encrypt(password);
+        System.out.println("加密:：" + encryptPwd);
+        String decrypt = encryptor.decrypt("icfpi+TeRVOnq2lkw7QGqg==");
+        System.out.println(decrypt);
     }
 
 }
