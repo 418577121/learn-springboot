b833f7d970e90dde84163392a80af3277b885bb5
iwin <418577121@qq.com>
2021-08-18 22:01:41 +0800

    springboot-003 init

---
+++ springboot-003/src/main/java/com/iwin/Springboot003Application.java
@@ -0,0 +1,13 @@
+package com.iwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot003Application.class, args);
+    }
+
+}
---
+++ springboot-003/src/test/java/com/iwin/Springboot003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot003ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
